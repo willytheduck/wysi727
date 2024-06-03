115dfdc64356ce0592cbb3ae1534073299bf6560
willytheduck <willycarlim39@example.com>
2024-06-03 18:27:44 +0700

    adding stuf

---
+++ script.js
@@ -0,0 +1,15 @@
+var btnClose = document.querySelector("span");
+var btnSubmit = document.querySelector(".submit");
+var modal = document.querySelector(".modal-container");
+if (btnSubmit) {
+  btnSubmit.addEventListener("click", function () {
+    modal.classList.add("show");
+    const next = document.querySelector(".next");
+    next.setAttribute("value", "https://theducky.netlify.app/");
+  });
+}
+if (btnClose) {
+  btnClose.addEventListener("click", function () {
+    modal.style.visibility = "hidden";
+  });
+}
